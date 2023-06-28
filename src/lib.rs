954d300bdad6da1b2417eb8c0f18927d7cb111d5
asakhar <a@sakhar.ru>
2023-06-28 12:30:14 +0300

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use proc_macro::{TokenStream, TokenTree};
+use quote::quote;
+
+extern crate proc_macro;
+
+#[proc_macro]
+pub fn concat_idents(ts: TokenStream) -> TokenStream {
+  let mut buf = String::new();
+  for t in ts {
+    let TokenTree::Ident(ident) = t else {
+      let t = t.to_string();
+      return quote! {
+        compile_error!(concat!("Ident expected. Found: '", #t, "'"));
+      }.into();
+    };
+    buf += &ident.to_string();
+  }
+  quote!(buf).into()
+}
